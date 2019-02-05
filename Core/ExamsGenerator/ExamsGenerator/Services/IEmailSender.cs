d5cab85460920cc41908c63c990c970028c25540
Andreea Popa <andpopa@outlook.com>
2019-02-05 09:44:42 +0200

    mvc core and identity - no db model

---
+++ Core/ExamsGenerator/ExamsGenerator/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExamsGenerator.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
